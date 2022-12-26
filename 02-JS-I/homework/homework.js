04183841998ab5961d915d11c4bda16f50cc9227
osvaldo <osvaldonovelli3@gmail.com>
2022-12-26 16:43:06 -0500

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -239,9 +239,9 @@ function esVocal(letra){
   if( letra.length < 1){
     return "Dato incorrecto"
   }
-  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"); {
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal"
-  }
+  } 
   return "Dato incorrecto" 
 }
 
