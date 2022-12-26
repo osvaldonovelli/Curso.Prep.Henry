8a04b5528383f1fc29d202d751652755a367be17
osvaldo <osvaldonovelli3@gmail.com>
2022-12-26 16:37:14 -0500

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -199,7 +199,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' + nombre + '!';
+  return 'Hola ' + nombre + '!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -230,17 +230,16 @@ function deEuroAdolar(euro){
   return euro * 1.2
 }
 
-
 function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if ( letra.length < 1){
+  if( letra.length < 1){
     return "Dato incorrecto"
   }
-  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u");{
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"); {
     return "Es vocal"
   }
   return "Dato incorrecto" 
