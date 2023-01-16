ddaf182fee43fc6ab0b437a737cef34cde3106e8
osvaldo <osvaldonovelli3@gmail.com>
2023-01-16 10:21:25 -0500

    firts commit

---
+++ 000-practicas/OperadoreLogicos.js
@@ -0,0 +1,6 @@
+function MayorYMenor(num){
+    if (num > 5 && num < 10 )console.log(true);
+    else console.log(false);
+};
+MayorYMenor(2);
+MayorYMenor(7);
---
+++ 000-practicas/arrays.js
@@ -0,0 +1,8 @@
+var ListadeCompras = [];
+ListadeCompras [3] = 'Tomates';
+ListadeCompras [1] = 'Lechuga';
+ListadeCompras [0] = 'zanahoria'
+//console.log(ListadeCompras);
+//ListadeCompras[1];
+var ElementoDelArray = ListadeCompras[1];
+console.log(ElementoDelArray); 
