b0acb8d32600d889128cc8d49a7d53a23fe1cae2
osvaldo <osvaldonovelli3@gmail.com>
2023-01-17 16:27:26 -0500

    Firts commit

--- 000-practicas/arrays.js
+++ 000-practicas/arrays.js
@@ -1,8 +1,81 @@
-var ListadeCompras = [];
-ListadeCompras [3] = 'Tomates';
-ListadeCompras [1] = 'Lechuga';
-ListadeCompras [0] = 'zanahoria'
-//console.log(ListadeCompras);
-//ListadeCompras[1];
-var ElementoDelArray = ListadeCompras[1];
-console.log(ElementoDelArray); 
+// var ListadeCompras = [];
+// ListadeCompras [3] = 'Tomates';
+// ListadeCompras [1] = 'Lechuga';
+// ListadeCompras [0] = 'zanahoria'
+// console.log(ListadeCompras);
+// ListadeCompras[1];
+// var ElementoDelArray = ListadeCompras[1];
+// console.log(ElementoDelArray); 
+
+//var Palabra = 'Henri';
+//var PalabraSeparada = Palabra.split('');
+//PalabraSeparada.pop();
+//PalabraSeparada.push('y');
+// console.log(PalabraSeparada);
+//var palabraArreglada = PalabraSeparada.join('');
+//console.log(palabraArreglada);
+ 
+
+//var numeros = [1,2,3,4]; 
+//numeros.forEach((num)=> console.log(num));
+//numeros.forEach((num) => {
+//    if (num === 3) {
+//                console.log(num);
+//    }
+// });
+
+//var masUno = numeros.map((num) => {
+//    return num + 1; 
+//    });
+//    console.log(masUno);
+
+//bucles FOR
+// var arr = [1, 2, 3, 4, 5];
+
+//for (var i = 0; i < arr.length; i++) {
+//    console.log(arr[i]); 
+// }
+
+//function encontrarLetraP(string) {
+//    var letras = string.split('');
+//
+//    for (let i = 0; i < letras.length; i++) {
+//        if(letras[i] === 'p') {
+//            console.log('si contiene a p'); 
+//        }
+//    }
+// }
+
+// encontrarLetraP('JavaScript');
+// encontrarLetraP('Henry');
+
+
+// While bucles:
+
+
+// var arr = [];
+
+// while (arr.length < 5) {
+//    arr.push('BOOM');
+// }
+// console.log(arr);
+
+//var persona ={Nombre: 'Osvaldo', Edad: 25, Estudios: {EsProgramador: true} };
+
+// console.log(persona.Edad);
+//persona.Nombre = 'Diana';
+// console.log(persona.Nombre);
+//persona.Edad = 38;
+// console.log(persona.Edad);
+
+//var autos= {};
+//autos.marcas = ['Ford' , 'Audi','Ferrari'];
+//delete autos.marcas;
+//console.log(autos);
+
+var MisFunciones = {
+    Saludar: function() {
+        console.log('Hola');
+    },
+};
+MisFunciones.Saludar();
